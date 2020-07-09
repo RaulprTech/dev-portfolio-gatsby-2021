ff777c7732f7188e713b7fec0dc5aa0dc3600eb4
Raul Pacheco Rodriguez <raul_vrm_2134@hotmail.com>
2020-07-09 18:29:14 -0500

    form change

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
     return(
-    <form className="mt-16 text-center" netlify>
+    <form className="mt-16 text-center" name="contact" method="POST" data-netlify="true">
         <label 
             htmlFor="contact-content" 
             className="block text-gray-700 text-sm font-bold mb-2">
