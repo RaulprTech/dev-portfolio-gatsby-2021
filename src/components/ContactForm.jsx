5dbf63f30d4c505bf120c9dd5300900b5e537943
Raul Pacheco Rodriguez <raul_vrm_2134@hotmail.com>
2020-07-09 19:11:49 -0500

    form-gatsby

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
 export default () => {
+
+    const [state, setState] = React.useState({})
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': form.getAttribute('name'),
+                ...state,
+            }),
+        })
+            .then(() => navigate(form.getAttribute('action')))
+            .catch((error) => alert(error))
+    }
     return(
-    <form className="mt-16 text-center" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    <form 
+        className="mt-16 text-center" 
+        name="contact" 
+        method="post" 
+        action="/thanks/"
+        data-netlify="true" 
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}>
         <label 
             htmlFor="contact-content" 
             className="block text-gray-700 text-sm font-bold mb-2">
@@ -12,7 +36,7 @@ export default () => {
             <textarea
                 id="contact-content"
                 type="hidden"
-                name="contact-content" 
+                name="contact-content"
                 className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
             >
             </textarea>
