5cfe8b00bc9606ffa04c99e14e364889acb13bef
Raul Pacheco Rodriguez <raul_vrm_2134@hotmail.com>
2020-07-10 16:56:38 -0500

    form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -56,16 +56,27 @@ export default () => {
           <label>
             Your name:
             <br />
-            <input type="text" name="name" onChange={handleChange} />
+            <input 
+                type="text"
+                name="name" 
+                onChange={handleChange}
+                className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" 
+            />
           </label>
         </p>
         <p>
           <label>
             Your email:
             <br />
-            <input type="email" name="email" onChange={handleChange} />
+            <input 
+                type="email" 
+                name="email" 
+                onChange={handleChange}
+                className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" 
+            />
           </label>
         </p>
+        <br/>
         <div className="flex shadow rounded bg-white border p-2">
             <textarea
                 id="message"
