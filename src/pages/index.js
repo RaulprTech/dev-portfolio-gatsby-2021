edc13256b6947122fb2e0c56bedf16fe5ed09fe1
raulprtech <raul_vrm_2134@hotmail.com>
2021-04-17 19:09:06 -0500

    section projects less

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
-import React from "react";
-import Header from '../components/Header';
-import Repos from '../components/Repos';
-import Skills from "../components/Skills";
-import Medium from "../components/Medium";
-import EduNav from "../components/Education-nav";
-import Proyects from '../components/Proyects';
-import TopProyects from "../components/TopProyects";
+import React from "react"
+import Header from "../components/Header"
+import Repos from "../components/Repos"
+import Skills from "../components/Skills"
+import Medium from "../components/Medium"
+import EduNav from "../components/Education-nav"
+import Proyects from "../components/Proyects"
+import TopProyects from "../components/TopProyects"
 
 export default function Home() {
-  return(
-      <div>
-        <Header/>
-        <Proyects/>
-        <TopProyects/>
-        <Repos/>
-        <EduNav/>
-        <Medium/>
-      </div>
+  return (
+    <div>
+      <Header />
+      <TopProyects />
+      <Repos />
+      <EduNav />
+      <Medium />
+    </div>
   )
 }
