71caa511084073f452746cffccd87a32494ea194
Raul Pacheco Rodriguez <35045085+RaulprTech@users.noreply.github.com>
2022-11-23 12:07:38 -0600

    update link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 export default function Footer() {
     return (
         <section className="container py-6 -px-4 mx-auto text-center">
-            © 2021 Created by <Link to="raulprtech.netlify.app/">Raul Pacheco</Link>
+            © 2021 Created by <Link to="https://raulpacheco.dev/">Raul Pacheco</Link>
         </section>
     )
 }
